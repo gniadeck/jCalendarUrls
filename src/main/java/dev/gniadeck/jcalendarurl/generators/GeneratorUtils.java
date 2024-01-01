c0df40e83c6d0e1976cf8342d78642433958a3f6
gniadeck <77535280+gniadeck@users.noreply.github.com>
2024-01-01 13:46:58 +0100

    Make GeneratorUtils package-private

--- src/main/java/dev/gniadeck/jcalendarurl/generators/GeneratorUtils.java
+++ src/main/java/dev/gniadeck/jcalendarurl/generators/GeneratorUtils.java
@@ -8,7 +8,7 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class GeneratorUtils {
+class GeneratorUtils {
 
     static LocalDateTime offsetToUtc(ZonedDateTime dateTime) {
         return dateTime.withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime();
